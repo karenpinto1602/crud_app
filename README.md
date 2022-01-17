7c2a80312c1bd2dcb702eb6cbffe0b21bcf97398
Karen Pinto <karenpinto1602@gmail.com>
2022-01-17 18:34:33 +0530

    user table styles added

---
+++ README.md
@@ -0,0 +1,4 @@
+1. create HTTP server in server.js
+2. add modules(body-parser)
+3. create views
+4. update changes in HTMl using "live server" package (here installed in VSC)
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT||8080;
+
+//log requests
+app.use(morgan('tiny')) //will print type of requests
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}))
+
+//set view engine
+app.set("view engine","ejs"); //can also use HTMl or pug in place of ejs
+//app.set("views",path.resolve(__dirname,"views/eje")); //if a separate folder with all eje files are made, here aren't doing that
+
+//load asset
+app.use('/css',express.static(path.resolve(__dirname,"assests/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assests/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assests/js")))
+
+app.get('/',(req,res)=>{
+    //res.send("Crud Application");
+    res.render('index')
+});
+
+app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)});
